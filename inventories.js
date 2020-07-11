0d2f22a26b3bd1396e7a9d71bcd2ec8ac9444dc8
Ashiqul Hoque Chowdhury <ashiqulhoque45@gmail.com>
2020-07-11 19:41:11 +0600

    invetories added

--- inventories.js
+++ inventories.js
@@ -1,5 +1,10 @@
 let inventory = {
-    "Pencil": 10,
-    "Eraser": 20
+    "Chicken Kabsah": 20,
+    "Jeresh": 30,
+    "Marqoq": 25,
+    "Waraq enab": 15,
+    "Humus": 10,
+    'Baba ganosh': 15,
+    "Salat": 5,
 }
 module.exports = (inventory) //to export json data
